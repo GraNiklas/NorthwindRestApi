18b8d111e7959fbb1ff0b5dce7b2442e24803f1a
Niklas Granqvist <niklasgranqvist@windowslive.com>
2025-08-18 14:08:53 +0300

    Add project files.

---
+++ NorthwindRestApi/Controllers/CustomersController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NorthwindRestApi.Models;
+
+namespace NorthwindRestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        NorthwindOriginalContext db = new();
+
+        [HttpGet]
+        public ActionResult GetAllCustomers()
+        {
+            var customers = db.Customers.ToList();
+            return Ok(customers);
+        }
+    }
+}
