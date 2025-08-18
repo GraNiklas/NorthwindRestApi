d639833d73549ebe44270c68cb80c7ce9ef509e6
Niklas Granqvist <niklasgranqvist@windowslive.com>
2025-08-18 14:46:14 +0300

    uusia metodeita customers controlleriin

--- NorthwindRestApi/Controllers/CustomersController.cs
+++ NorthwindRestApi/Controllers/CustomersController.cs
@@ -13,8 +13,51 @@ namespace NorthwindRestApi.Controllers
         [HttpGet]
         public ActionResult GetAllCustomers()
         {
-            var customers = db.Customers.ToList();
-            return Ok(customers);
+            try
+            {
+                var customers = db.Customers.ToList();
+                return Ok(customers);
+
+            }
+            catch (Exception e)
+            {
+                return BadRequest($"Error: {e.Message}");
+                throw;
+            }
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult GetOneCustomerById(string id)
+        {
+            try
+            {
+                var customer = db.Customers.Find(id);
+                if(customer == null)
+                {
+                    return NotFound($"Customer {id} not found.");
+                }
+                return Ok(customer);
+            }
+            catch (Exception e)
+            {
+                return BadRequest("Tapahtui virhe: " + e);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult CreateCustomer([FromBody] Customer customer)
+        {
+            try
+            {
+                db.Customers.Add(customer);
+                db.SaveChanges();
+                return Ok("Uusi asiakas lisätty: " + customer.CompanyName);
+            }
+            catch (Exception e)
+            {
+                return BadRequest("Errormessage: " + e.Message);
+                throw;
+            }
         }
     }
 }
