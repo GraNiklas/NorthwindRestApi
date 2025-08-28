fc9cb8f8c1bb976ccae03a277eb432caaf2c5e7d
Niklas Granqvist <niklasgranqvist@windowslive.com>
2025-08-28 21:10:07 +0300

    julkaisuun menevä

--- NorthwindRestApi/Program.cs
+++ NorthwindRestApi/Program.cs
@@ -10,7 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddDbContext<NorthwindOriginalContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("paikallinen")));
+builder.Services.AddDbContext<NorthwindOriginalContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("azure")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
